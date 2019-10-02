98a4d8deef4acdfed8e8708db14c9972d1185c3c
josex2r <jlrepresa@gmail.com>
2019-10-02 19:37:27 +0200

    add token cookie

--- routes/middlewares/auth.js
+++ routes/middlewares/auth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   if (req.session && req.session.user) {
     res.locals.user = req.session.user;
+    res.cookie('token', req.session.user.token);
     return next();
   } else {
     return res.redirect('/');
